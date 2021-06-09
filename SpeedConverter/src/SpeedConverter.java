3b95a5514c7187c38310dc7d2384c348710c573e
dorukanozercan <42218198+dorukanozercan@users.noreply.github.com>
2021-06-09 23:28:17 +0300

    added files

---
+++ SpeedConverter/src/SpeedConverter.java
@@ -0,0 +1,27 @@
+
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+        // can take any value except less than zero
+        printConversion(10.25);
+    }
+
+    public static long toMilesPerHour (double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            long result = Math.round((kilometersPerHour / 1.609));
+            return result;
+        }
+    }
+
+    public static void printConversion (double kilometersPerHour) {
+        long miles = toMilesPerHour(kilometersPerHour);
+        if (miles == -1) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + miles + " mi/h");
+        }
+
+    }
+}
